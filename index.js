c03cdbcb4ed39f17c201228de95c8fc9cd9b9800
Dipen Shah <dipen08it419@gmail.com>
2015-03-17 02:22:06 -0700

    Starting again.
    I had to remove files again to solve the issue generated by angular and
    express routing system.

--- index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-var app = express()
-
-app.listen(1337);
-
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-  res.send('hello world')
-})
